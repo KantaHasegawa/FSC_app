889118933e2f7acbfdb680dc63f6bfec266f4f48
KantaHasegawa <gyoza20000@gmail.com>
2020-08-16 12:53:16 +0900

    ユーザー機能完成

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
+  get '/users/sign_out' => 'devise/sessions#destroy'
+  resources :users, :only => [:index,:show]
+  root to: 'static_pages#home'
   get 'static_pages/home'
-  get 'static_pages/index'
-  get 'static_pages/show'
   get 'static_pages/help'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   #
-  root to: 'static_pages#home'
-  get '/users/sign_out' => 'devise/sessions#destroy'
+
+
 end
